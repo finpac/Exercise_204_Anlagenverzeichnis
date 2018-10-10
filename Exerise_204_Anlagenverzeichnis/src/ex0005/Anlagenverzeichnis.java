cefb5fcbbbf66c8c6cd0b28394366d881647591d
Fink Patrick <finpac14@htlkaindorf.at>
2018-10-10 10:10:46 +0200

    Klasse Anlangenverzeichnis implementiert

--- Exerise_204_Anlagenverzeichnis/src/ex0005/Anlagenverzeichnis.java
+++ Exerise_204_Anlagenverzeichnis/src/ex0005/Anlagenverzeichnis.java
@@ -1,8 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.finpac14;
 
-package ex0005;
+/**
+ *
+ * @author Patrick
+ */
+public class Anlagenverzeichnis {
+    private String bezeichnung;
+    private double anschaffungswert;
+    private double inbetriebnahme;
+    private double nutzungsdauer;
+    private double bihNutzungsdauer;
+    private double AfABisher;
+    private double WertVorAfA;
+    private double afAdJ;
+    private double buchwert;
 
+    public Anlagenverzeichnis(String bezeichnung, double anschaffungswert, double inbetriebnahme, double nutzungsdauer) {
+        this.bezeichnung = bezeichnung;
+        this.anschaffungswert = anschaffungswert;
+        this.inbetriebnahme = inbetriebnahme;
+        this.nutzungsdauer = nutzungsdauer;
+    }
 
-public class Anlagenverzeichnis
-{
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public double getAnschaffungswert() {
+        return anschaffungswert;
+    }
+
+    public double getInbetriebnahme() {
+        return inbetriebnahme;
+    }
+
+    public double getNutzungsdauer() {
+        return nutzungsdauer;
+    }
+
+    public double getBihNutzungsdauer() {
+        return bihNutzungsdauer;
+    }
+
+    public double getAfABisher() {
+        return AfABisher;
+    }
+
+    public double getWertVorAfA() {
+        return WertVorAfA;
+    }
+
+    public double getAfAdJ() {
+        return afAdJ;
+    }
+
+    public double getBuchwert() {
+        return buchwert;
+    }
+
+    public void setBihNutzungsdauer(double bihNutzungsdauer) {
+        this.bihNutzungsdauer = bihNutzungsdauer;
+    }
+
+    public void setAfABisher(double AfABisher) {
+        this.AfABisher = AfABisher;
+    }
+
+    public void setWertVorAfA(double WertVorAfA) {
+        this.WertVorAfA = WertVorAfA;
+    }
+
+    public void setAfAdJ(double afAdJ) {
+        this.afAdJ = afAdJ;
+    }
+    
     
 }
